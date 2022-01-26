48caff5c25711e2b7cdfca8d6e6bf4188079614e
MarcosVOS <Marcos.OliveiraDEV@gmail.com>
2022-01-25 21:55:46 -0300

    add cors

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const PORT = 3000
 const express = require('express')
 const path = require("path")
 const apiRoute = require('./routes/api') 
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use('/api',apiRoute)
 app.use('/',express.static(path.join(__dirname,"public")))
 
-app.listen(PORT, ()=>{
-  console.log('Server running on port',PORT)
-})
-
+app.listen(process.env.PORT || PORT)
